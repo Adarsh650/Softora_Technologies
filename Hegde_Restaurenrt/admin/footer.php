54158bf3dc5be8ae5fefeebb5645a5557a9c83d5
Adarsh Hegde <69852336+Adarsh650@users.noreply.github.com>
2021-11-16 09:28:27 +0530

    Add files via upload

---
+++ Hegde_Restaurenrt/admin/footer.php
@@ -0,0 +1,2 @@
+<br><br>
+<div class="footer" style="background-color: #ccc;color:black;font-size:15px;"> Designed By:Adarsh Hegde</div>
---
+++ Hegde_Restaurenrt/admin/header.php
@@ -0,0 +1,12 @@
+<link rel="stylesheet" type="text/css" href="style2.css">
+<div class="h" style="background-color:#babfa8;" align="center"><b>HEGDE RESTAURENT</div>
+<div class="mbg">
+	<a href="after_login.php" class="menu" style="background-color:#aebd7b; ">Parcel Clients</a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	<a href="food.php" class="menu" style="background-color:#aebd7b; ">Food Menu</a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	<a href="gallery.php" class="menu" style="background-color:#aebd7b; ">Gallery</a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	<a href="review.php" class="menu" style="background-color:#aebd7b; ">Reviews</a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	<a href="food_cat.php" class="menu" style="background-color:#aebd7b;">Food Category</a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	<a href="logout.php" class="menu" style="background-color:#aebd7b; color: red;">Logout</a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	
+</div>
+<br><br>
